da2037bc029920af882485448f50abfac7236e40
MichaelJustHansen <git@justhansen.com>
2015-09-15 11:24:59 +0200

    Added plugin source files

---
+++ index.js
@@ -0,0 +1,21 @@
+var _exec = require('child_process');
+var _intervalSeconds = parseInt(process.argv[3]) || 1;
+var _process = process.argv[2].toLowerCase();
+
+function poll()
+{
+  var tasklist = _exec.execSync('tasklist').toString('ascii').toLowerCase();
+  var process_exists;
+
+  if(tasklist.indexOf(_process) > -1) 
+    process_exists = 1;
+  else 
+    process_exists = 0;
+
+//  console.log('PARAMS: %s - %d', _process, _intervalSeconds);
+  console.log('PROC_EXIST %d', process_exists );
+
+  setTimeout(poll, _intervalSeconds * 1000);  // param in milliseonds
+}
+
+poll();
