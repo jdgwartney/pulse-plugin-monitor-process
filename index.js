c504a5e755890adf2c13613b49a5cf86a667eb09
MichaelJustHansen <michael@justhansen.com>
2015-10-06 09:24:10 +0200

    Updated for Linux

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
 var _exec = require('child_process');
+var _os = require('os');
 var _interval;
 var _process;
+var _OSType;
 
 function poll() 
 {
   if(_process) 
   {
-    var tasklist = _exec.execSync('c:/windows/system32/tasklist.exe').toString('ascii').toLowerCase();
-    var process_exists;
+    var tasklist;
+    if(_OSType = 'windows_nt')
+      tasklist = _exec.execSync('c:/windows/system32/tasklist.exe').toString('ascii').toLowerCase();
+    else
+      tasklist = _exec.execSync('ps -e').toString('ascii').toLowerCase();
 
+    var process_exists;
     if(tasklist.indexOf(_process) > -1) 
       process_exists = 1;
     else 
@@ -21,7 +27,8 @@ function poll()
     var paramData = require(__dirname + '/param.json');
     _interval = parseInt(paramData['Poll interval']);
     _process = paramData['Process name'].toLowerCase();
-//    console.log('Interval: %d\nProcess: %s', _interval, _process)
+    _OSType = _os.type().toString('ascii').toLowerCase();
+    console.log('Interval: %d\nProcess: %s\nOS: %s', _interval, _process, _OSType)
   }
 
   setTimeout(poll, _interval);  // param in milliseonds
